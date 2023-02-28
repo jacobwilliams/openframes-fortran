b1509bab79135efb98833bd40a393c46002a2d44
Jacob Williams <jacobwilliams@users.noreply.github.com>
2023-02-28 17:14:22 -0600

    ninja version of build script

--- experiments/build_openframes.sh
+++ experiments/build_openframes.sh
@@ -11,6 +11,7 @@ export CMAKE_VERSION=3.25
 export SWIG_VERSION=4.0
 export NUMPY_VERSION=1.24
 export PYQT_VERSION=5.15
+export NINJA_VERSION=1.11
 
 # magic to make conda work:
 eval "$(conda shell.bash hook)"
@@ -20,12 +21,19 @@ rm -rf ./build
 mkdir ./build
 
 # create the conda environment and activate it:
-conda create --channel nodefaults --channel conda-forge --prefix ./build/osgenv --solver libmamba python=$PYTHON_VERSION openscenegraph=$OSG_VERSION cmake=$CMAKE_VERSION swig=$SWIG_VERSION numpy=$NUMPY_VERSION PYQT=$PYQT_VERSION
+conda create --channel nodefaults --channel conda-forge --prefix ./build/osgenv --solver libmamba python=$PYTHON_VERSION openscenegraph=$OSG_VERSION cmake=$CMAKE_VERSION swig=$SWIG_VERSION numpy=$NUMPY_VERSION PYQT=$PYQT_VERSION ninja=$NINJA_VERSION
 cd build
 conda activate ./osgenv
 
 # configure:
-cmake -D OSG_DIR=./osgenv -D OF_BUILD_DEMOS=ON -D OF_PYTHON_MODULE=ON -D SWIG_EXECUTABLE=./osgenv/bin/swig ..
+cmake -G Ninja \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D OSG_DIR=./osgenv \
+      -D OF_BUILD_DEMOS=ON \
+      -D OF_PYTHON_MODULE=ON \
+      -D SWIG_EXECUTABLE=./osgenv/bin/swig \
+      ..
 
 # build:
-make -j 4 install
+ninja
+ninja install
